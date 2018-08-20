7c465c46a9ca6c14aeed1d8b6e2813ca1cb770ff
Michel G. Souza <Michelgomesdes@hotmail.com>
2018-08-20 15:37:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -15,12 +15,26 @@ class Fita:
         self.conteudo[self.pos] = novo
     def mover(self, novo, direcao):
         direcao.upper()
-        if direcao == 'R':
-            self.setConteudo(novo)
+    
+        if direcao == 'R': 
+            tmp = self.pos+ 1
+            if(tmp > len(self.conteudo) ): #Caso A Maquina Queira adicionar Algo na Frente do Conteudo
+                self.Conteudo.append(novo)
+                self.Conteudo.append('B')
+            else:
+                self.setConteudo(novo)
+
             self.pos += 1
+
         elif direcao == 'L':
-            self.setConteudo(novo)
-            self.pos -= 1
+            tmp = self.pos-1
+            if(tmp < 0):
+                self.conteudo.insert(0,novo)
+                print('aa')
+                self.pos -= 1
+            else:
+                self.setConteudo(novo)
+                self.pos -= 1
 
 
 class Transicao:
@@ -78,17 +92,17 @@ def run(control, fita):
     parada = 1
     #fita.mover('B', 'R')
     while(parada):
-        print(fita)
         controle = control.estados[control.getInicio()]
         for i in range(len(control.fim)):
             if control.estados[control.getInicio()].getNome() == str(control.fim[i]): #Se o Inicio esta em um dos Estados de Fim
                 parada = 0
+                print("ENCONTROU O ESTADO FINAL [%s]"%(control.fim[i]))
                 break
         if parada != 0:
+            print(fita)
             for i in range(len(controle.trans)):
                 parada = 0; #Linha Caso o IF abaixo não encontre o dado no Estado Atual, ira Crashar/Sair
                 if fita.getConteudo() == controle.trans[i].getDado() :
-                    #print('VALOR DO I [%d]'%(i))
                     fita.mover(controle.trans[i].getEscrever(),controle.trans[i].getDirecao())
                     control.setInicio(controle.trans[i].getNextState())
                     print('Estado Atual [%d] -Antigo [%s] -  Foi Escrito [%s] - A Direção [%s]'%(control.getInicio(),controle.trans[i].getDado(), controle.trans[i].getEscrever(),controle.trans[i].getDirecao()))
@@ -116,7 +130,9 @@ def setup():
     if count != len(entrada):
         print("ENTRADA NAO VALIDA")
         sys.exit()
+
     fita = Fita(entrada, 0, alfabeto)
+    fita.conteudo.append('B') #Adiciona um Branco no Final
     line = f.readline()
     line = f.readline() # linha 4 conjunto de estados
     line = line.replace("\n", "")
