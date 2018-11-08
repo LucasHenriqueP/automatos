42d043955a414f2be8388f9eb76dbec51bc88e91
Michel G. Souza <michels@alunos.utfpr.edu.br>
2018-11-08 11:29:24 -0200

    getting text parameters

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ def setup():
     entrada = entrada.replace(" ", "")
     f = open(arq, 'r')
     alfabetoEntrada = f.readline() #Linha 1 - Alfabeto de Entrada
+    
+    epsilon = f.readline() #Linha 2 - Símbolo a ser considerado para representar epsilon ou lambda
+    
+    line = f.readline() #Linha 3 - Conjunto de Estados
+    line = line.replace("\n", "")
+    estados = line.split(" ")
+    estados.sort() #Ordena os Estados
+    
+    line = f.readline() #Linha 4 - Estado Inicial
+    estadoInicio = line.replace("\n", "")
+    
+    line = f.readline() #Linha 5 - Conjunto de Estados de Aceitação
+    line = line.replace("\n", "")
+    estadosAceitacao = line.split(" ")
+    estadosAceitacaos.sort() #Ordena os Estados
